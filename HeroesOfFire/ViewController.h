fa828e2bb898b2a10ac7c1b6f2e54ef29d59380c
Amir Bahadori <pbahadori156@gmail.com>
2016-08-01 18:10:55 -0700

    Initial Commit

---
+++ HeroesOfFire/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HeroesOfFire
+//
+//  Created by Pasha Bahadori on 8/1/16.
+//  Copyright © 2016 Pelican. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
